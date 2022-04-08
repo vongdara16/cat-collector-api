77f32a75a5c29d068c40957057003d753dea2697
Justin Vongdara <vongdara16@gmail.com>
2022-04-08 17:20:39 -0400

    cats and toys crud complete

--- api/__init__.py
+++ api/__init__.py
@@ -7,9 +7,13 @@ from config import Config
 # ============ Import Models ============
 from api.models.user import User
 from api.models.profile import Profile
+from api.models.cat import Cat
+from api.models.toy import Toy
 
 # ============ Import Views ============
 from api.views.auth import auth
+from api.views.cats import cats
+from api.views.toys import toys
 
 cors = CORS()
 migrate = Migrate() 
@@ -25,6 +29,8 @@ def create_app(config):
 
   # ============ Register Blueprints ============
   app.register_blueprint(auth, url_prefix='/api/auth') 
+  app.register_blueprint(cats, url_prefix='/api/cats')
+  app.register_blueprint(toys, url_prefix='/api/toys')
 
   return app
 
---
+++ api/views/cats.py
@@ -0,0 +1,61 @@
+from email import message
+import json
+import profile
+from flask import Blueprint, jsonify, request
+from api.middleware import login_required, read_token
+
+from api.models.db import db
+from api.models.cat import Cat
+
+cats = Blueprint('cats', 'cats')
+
+@cats.route('/', methods=['POST'])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data['profile_id'] = profile['id']
+  cat = Cat(**data)
+  db.session.add(cat)
+  db.session.commit()
+  return jsonify(cat.serialize()), 201
+
+@cats.route('/', methods=['GET'])
+def index():
+  cats = Cat.query.all()
+  return jsonify([cat.serialize() for cat in cats]), 200
+
+@cats.route('/<id>', methods=['GET'])
+def show(id):
+  cat = Cat.query.filter_by(id=id).first()
+  cat_data = cat.serialize()
+  return jsonify(cat=cat_data), 200
+
+@cats.route('/<id>', methods=['PUT'])
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  cat = Cat.query.filter_by(id=id).first()
+
+  if cat.profile_id != profile['id']:
+    return 'Forbidden', 403
+  
+  for key in data:
+    setattr(cat, key, data[key])
+  
+  db.session.commit()
+  return jsonify(cat.serialize()), 200
+
+@cats.route('/<id>', methods=['DELETE'])
+@login_required
+def delete(id):
+  profile = read_token(request)
+  cat = Cat.query.filter_by(id=id).first()
+
+  if cat.profile_id != profile['id']:
+    return 'Forbidden', 403
+  
+  db.session.delete(cat)
+  db.session.commit()
+  return jsonify(message='Success'), 200
